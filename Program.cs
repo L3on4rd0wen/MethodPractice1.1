b4ea16d138116a00247c85ace57595e48d26e4d3
L3on4rd0wen <l3on4rd30@gmail.com>
2026-01-15 14:28:05 +0700

    dynamic functions

--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
-﻿int num1 = 10, num2 = 20, result = 0; 
+﻿int result = 0; 
 
-void Add()
+void Add(int num1, int num2)
 { 
     result = num1 + num2; 
     Console.WriteLine($"{num1} + {num2} = {result}");
 }
 
-void Sub()
+void Sub(int num1, int num2)
 {
     result = num1 - num2;
     Console.WriteLine($"{num1} - {num2} = {result}");
 }
 
-void Mult()
+void Mult(int num1, int num2)
 {
     result = num1 * num2;
     Console.WriteLine($"{num1} * {num2} = {result}");
 }
 
-void Over()
+void Over(int num1, int num2)
 {
     result = num1 / num2;
     Console.WriteLine($"{num1} / {num2} = {result}");
 }
 
-Add();
-Sub();
-Mult();
-Over();
+Add(12, 16);
+Sub(10, 7);
+Mult(5, 2);
+Over(50, 5);
--- obj/Debug/net8.0/MethodPractice1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MethodPractice1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodPractice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92adda39228114273b841b7e62d7b50bcb6991d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodPractice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodPractice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
